f2e6b546be15754ff659935d7b8f291d7e2074d8
Manoj Reddy <manojeee251@gmail.com>
2023-05-08 09:19:53 +0530

    Update dockerpush.sh

--- dockerpush.sh
+++ dockerpush.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# To build, tag and push image below is the command
+#  bash dockerpush.sh REPO_lINK VERSION
+# example : bash dockerpush.sh manoj0251/build_push_docker 2.3.1
+# You can manually update the REPO if need
 
-REPO="manoj0251/build_push_docker"
-VERSION=$1
+VERSION=$2
+REPO=$1
 echo "Building the docker Image for version:"$VERSION
 docker build -t $REPO:$VERSION .
+# IF you a docker file with a different name , comment the above command and enable below command
+# docker build -t $REPO:$VERSION -f /path/to/Dockerfile .
 docker images
 IMAGE_ID=$(docker images --format "{{.ID}}" --no-trunc $REPO:$VERSION | cut -c8-19)
 echo $IMAGE_ID
